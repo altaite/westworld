a6c7fc27772e734c4f2ad0b9d28d793a83921627
Antonín Pavelka <antonin.pavelka@gmail.com>
2025-08-30 10:06:47 -0700

    Add Creature struct with movement and rotation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+mod creature;
+
+use creature::{Creature, Position};
+
 fn main() {
-    println!("Hello, world!");
+    let mut c = Creature::new(Position::new(0.0, 0.0), 0.0);
+    c.move_forward(0.5);
+    c.rotate(0.1);
+    println!("{:?}", c);
 }
